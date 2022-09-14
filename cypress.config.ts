7abcdac43cd8e1fd51d0f4cdb5656277e3900d2a
Austin Shelby <helloaustinshelby@gmail.com>
2022-09-14 12:06:28 +0200

    add custom task to seed database

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,24 @@
+import { prisma } from "./src/prisma";
 import { defineConfig } from "cypress";
+import type { Event } from "@prisma/client";
+
+export const seedEvents = async (events: Event[]): Promise<null> => {
+  await Promise.all(
+    events.map(async (event) => {
+      await prisma.event.create({ data: event });
+    })
+  );
+  return null;
+};
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    supportFile: false,
+    setupNodeEvents(on) {
+      on("task", {
+        "seed:events": seedEvents,
+      });
+    },
   },
   screenshotOnRunFailure: false,
   video: false,
--- cypress/e2e/meetup.cy.ts
+++ cypress/e2e/meetup.cy.ts
@@ -1,5 +1,16 @@
 describe("Meetup detail page", () => {
   it("Should exist when it is in database", () => {
+    cy.task("seed:events", [
+      {
+        id: 1,
+        name: "Test Meetup",
+        slug: "test-meetup",
+        description: "",
+        location: "Online",
+        dateTime: new Date(2030, 0, 1),
+      },
+    ]);
+
     cy.visit("/test-meetup");
   });
 });
