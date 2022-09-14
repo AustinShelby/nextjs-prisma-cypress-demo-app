8f7d56e3f67366735198e4edc4f9487b8e4afc39
Austin Shelby <helloaustinshelby@gmail.com>
2022-09-14 12:06:28 +0200

    add custom task to seed database

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,25 @@
+import { prisma } from "./src/prisma";
 import { defineConfig } from "cypress";
+import type { Event } from "@prisma/client";
+
+export const seedEvents = async (events: Event[]): Promise<null> => {
+  await Promise.all(
+    events.map(async (event) => {
+      await prisma.event.create({ data: event });
+    })
+  );
+  return null;
+};
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://0.0.0.0:3000",
     supportFile: false,
+    setupNodeEvents(on) {
+      on("task", {
+        "seed:events": seedEvents,
+      });
+    },
   },
   screenshotOnRunFailure: false,
   video: false,
