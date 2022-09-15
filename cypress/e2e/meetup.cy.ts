03ff28ca544c50b3a7543b09c5da842fff6bcf80
Austin Shelby <helloaustinshelby@gmail.com>
2022-09-15 14:07:14 +0200

    another test

--- cypress/e2e/meetup.cy.ts
+++ cypress/e2e/meetup.cy.ts
@@ -13,6 +13,21 @@ describe("Meetup detail page", () => {
 
     cy.visit("/test-meetup");
   });
+
+  it("Should exist also when it is in database", () => {
+    cy.task("seed:events", [
+      {
+        id: 1,
+        name: "Test Meetup 2",
+        slug: "test-meetup-2",
+        description: "",
+        location: "Online",
+        dateTime: new Date(2030, 0, 1),
+      },
+    ]);
+
+    cy.visit("/test-meetup-2");
+  });
 });
 
 export {};
