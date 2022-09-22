75b5c754b13076ccc8d1cd3e2c5e1b064164c1c5
Austin Shelby <helloaustinshelby@gmail.com>
2022-09-22 16:53:36 +0200

    add a new test

--- cypress/e2e/list-page.cy.ts
+++ cypress/e2e/list-page.cy.ts
@@ -39,6 +39,36 @@ describe("Meetups list page", () => {
     cy.visit("/");
     cy.get("ul li").should("have.length", 0);
   });
+
+  it("meetups are shown in order by dateTime in ascending order", () => {
+    cy.task("seed:events", [
+      {
+        id: 1,
+        name: "TypeScript Meetup",
+        slug: "typescript-meetup",
+        description: "",
+        location: "Online",
+        dateTime: new Date(2030, 11, 23),
+      },
+      {
+        id: 2,
+        name: "Python Meetup",
+        slug: "python-meetup",
+        description: "",
+        location: "Online",
+        dateTime: new Date(2030, 11, 24),
+      },
+    ]);
+
+    cy.visit("/");
+
+    cy.get("ul li")
+      .should("have.length", 2)
+      .first()
+      .should("contain", "TypeScript Meetup")
+      .next()
+      .should("contain", "Python Meetup");
+  });
 });
 
 export {};
