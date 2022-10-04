5de37fb8fd194bb66dd8680141ad7c6fb4cc3c6a
Austin Shelby <helloaustinshelby@gmail.com>
2022-10-04 15:50:55 +0200

    add tests

--- cypress/e2e/create-page.cy.ts
+++ cypress/e2e/create-page.cy.ts
@@ -8,6 +8,30 @@ describe("Meetup create page", () => {
 
     cy.contains("Add a new meetup");
   });
+
+  it("Meetup title and datetime input work", () => {
+    cy.visit("/create");
+
+    cy.get("input#name")
+      .should("have.value", "")
+      .type("test")
+      .should("have.value", "test");
+
+    cy.get("input#datetime")
+      .should("have.value", "")
+      .type("2030-10-10T10:10")
+      .should("have.value", "2030-10-10T10:10");
+  });
+
+  it("Submitting empty form gives error on required fields title, location and date", () => {
+    cy.visit("/create");
+
+    cy.get("button:submit").click();
+
+    cy.contains("Please enter a name for the meetup.");
+    cy.contains("Please enter a location for the meetup.");
+    cy.contains("Please enter a date that is in the future.");
+  });
 });
 
 export {};
