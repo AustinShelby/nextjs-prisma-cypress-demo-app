c153a293e12144155d0b99f60de7ddc887d4e1a5
Austin Shelby <helloaustinshelby@gmail.com>
2022-10-04 17:02:16 +0200

    create tests

--- cypress/e2e/create-page.cy.ts
+++ cypress/e2e/create-page.cy.ts
@@ -55,6 +55,66 @@ describe("Meetup create page", () => {
 
     cy.contains("New meetup created successfully!");
   });
+
+  it("Can't create a meetup if one with the same generated slug exists", () => {
+    cy.task("seed:events", [
+      {
+        id: 1,
+        name: "Doesn't matter",
+        slug: "typescript-meetup",
+        description: "",
+        location: "Online",
+        dateTime: new Date("2030-10-10T10:10"),
+      },
+    ]);
+
+    cy.visit("/create");
+
+    cy.get("input#name")
+      .should("have.value", "")
+      .type("TypeScript Meetup")
+      .should("have.value", "TypeScript Meetup");
+
+    cy.get("input#location")
+      .should("have.value", "")
+      .type("Online")
+      .should("have.value", "Online");
+
+    cy.get("input#datetime")
+      .should("have.value", "")
+      .type("2030-10-10T10:10")
+      .should("have.value", "2030-10-10T10:10");
+
+    cy.get("button:submit").click();
+
+    cy.contains("A meetup with this name already exists.");
+  });
+
+  it("A newly created meetup has a page", () => {
+    cy.visit("/create");
+
+    cy.get("input#name")
+      .should("have.value", "")
+      .type("TypeScript Meetup")
+      .should("have.value", "TypeScript Meetup");
+
+    cy.get("input#location")
+      .should("have.value", "")
+      .type("Online")
+      .should("have.value", "Online");
+
+    cy.get("input#datetime")
+      .should("have.value", "")
+      .type("2030-10-10T10:10")
+      .should("have.value", "2030-10-10T10:10");
+
+    cy.get("button:submit").click();
+
+    cy.contains("New meetup created successfully!");
+
+    cy.visit("/typescript-meetup");
+    cy.get("[data-cy='title']").contains("TypeScript Meetup");
+  });
 });
 
 export {};
