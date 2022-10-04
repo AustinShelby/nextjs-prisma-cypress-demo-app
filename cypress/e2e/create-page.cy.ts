3b2aabee70541ee0d54d0cdfd57d9d9369a98ba9
Austin Shelby <helloaustinshelby@gmail.com>
2022-10-04 16:45:23 +0200

    add test

--- cypress/e2e/create-page.cy.ts
+++ cypress/e2e/create-page.cy.ts
@@ -32,6 +32,29 @@ describe("Meetup create page", () => {
     cy.contains("Please enter a location for the meetup.");
     cy.contains("Please enter a date that is in the future.");
   });
+
+  it("Success message shows after a successful submit", () => {
+    cy.visit("/create");
+
+    cy.get("input#name")
+      .should("have.value", "")
+      .type("test")
+      .should("have.value", "test");
+
+    cy.get("input#location")
+      .should("have.value", "")
+      .type("Online")
+      .should("have.value", "Online");
+
+    cy.get("input#datetime")
+      .should("have.value", "")
+      .type("2030-10-10T10:10")
+      .should("have.value", "2030-10-10T10:10");
+
+    cy.get("button:submit").click();
+
+    cy.contains("New meetup created successfully!");
+  });
 });
 
 export {};
