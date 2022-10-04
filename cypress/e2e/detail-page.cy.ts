aa877451b02d15a4d384e51c9b35c2a0cf18b708
Austin Shelby <helloaustinshelby@gmail.com>
2022-10-04 12:55:19 +0200

    add test

--- cypress/e2e/detail-page.cy.ts
+++ cypress/e2e/detail-page.cy.ts
@@ -23,6 +23,26 @@ describe("Meetup detail page", () => {
       .its("status")
       .should("eq", 404);
   });
+
+  it("meetup data is formatted correctly", () => {
+    cy.task("seed:events", [
+      {
+        id: 1,
+        name: "TypeScript Meetup",
+        slug: "typescript-meetup",
+        description: "This is a description",
+        location: "Online",
+        dateTime: new Date(2030, 0, 1),
+      },
+    ]);
+
+    cy.visit("/typescript-meetup");
+
+    cy.get("[data-cy='title']").contains("TypeScript Meetup");
+    cy.get("[data-cy='description']").contains("This is a description");
+    cy.get("[data-cy='location']").contains("Online");
+    cy.get("[data-cy='date']").contains("January 1, 2030");
+  });
 });
 
 export {};
